8ce9e699f205cc57ffa9cdb6d1d3e384f8b1a23b
Suphasin Yosang <fusesuphasin@gmail.com>
2021-09-23 11:55:58 +0700

    add structure portfolio

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,34 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:0638419845">0638419845</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="gmail:suphasin.yosang@gmail.com">
+            suphasin.yosang@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project a time</Slogan>
+        </CompanyContainer>
+        <SocialIcons href="gttps://github.com">
+        <AiFillGithub size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="gttps://linkedin.com">
+        <AiFillInstagram size="3rem" />
+      </SocialIcons>
+      <SocialIcons href="gttps://instagram.com">
+        <AiFillLinkedin size="3rem" />
+      </SocialIcons>
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
